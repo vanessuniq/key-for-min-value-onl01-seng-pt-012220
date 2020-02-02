246c451865c53085f2dea73e5367157352cfe512
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-02 05:52:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,10 @@ def key_for_min_value(name_hash)
   value_array = []
   name_hash.each do |k, v| 
     value_array << v 
-    value.array.each {||}
+    value.array.each do |v|
+      if v == value_array.sort.first 
+        k 
+      end
+    end
   end 
 end 
